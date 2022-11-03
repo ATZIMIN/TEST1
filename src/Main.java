f51b6b6b47df9d1bdb57e2a8fbd6af29eca6fbad
ATZIMIN <hyjimin@gmail.com>
2022-11-03 16:23:12 +0900

    아 이거 참 어렵네요 허허

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("집에 가고 싶다");
+        System.out.println("안녕하세요 저는 깃을 오늘 처음합니다.");
     }
 }
